1729a53286972ec0a48dcbd137a9b333735f56b4
chandan632 <chandanorut737@gmail.com>
2020-08-10 12:33:53 +0530

    Updates

---
+++ precache-manifest.3c4f884636c350a39cdbb5c39f1ad8f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "151e1b2e73368d03cde478ce20094338",
+    "url": "/hello/index.html"
+  },
+  {
+    "revision": "5996a0ffa8d2d6647028",
+    "url": "/hello/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "eb6ffb3945c9bd6953b3",
+    "url": "/hello/static/js/2.72442180.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hello/static/js/2.72442180.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5996a0ffa8d2d6647028",
+    "url": "/hello/static/js/main.6f72d424.chunk.js"
+  },
+  {
+    "revision": "543a3730c717a6d3edaf",
+    "url": "/hello/static/js/runtime-main.eda9fcde.js"
+  }
+]);
